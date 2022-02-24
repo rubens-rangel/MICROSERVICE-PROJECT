b8e723d7153170ffd6694435ef1757eb9d4a7a61
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-02-24 14:08:09 -0300

    implementando resttemplate

---
+++ hr-payroll/src/main/java/com/rubens/hrpayroll/config/Appconfig.java
@@ -0,0 +1,15 @@
+package com.rubens.hrpayroll.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Appconfig {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
