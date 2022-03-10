119544389e40ee296640ab60df92e4c2a999c48f
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-03-10 16:22:24 -0300

    Repository, resources and config users

---
+++ hr-user/src/main/java/com/rubens/hruser/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.rubens.hruser.repositories;
+
+import com.rubens.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
---
+++ hr-user/src/main/java/com/rubens/hruser/resources/UserResource.java
@@ -0,0 +1,36 @@
+package com.rubens.hruser.resources;
+
+import com.rubens.hruser.entities.User;
+import com.rubens.hruser.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = repository.findById(id).get();
+        return ResponseEntity.ok(obj);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findById(@RequestParam String email) {
+        User obj = repository.findByEmail(email);
+        return ResponseEntity.ok(obj);
+    }
+
+}
