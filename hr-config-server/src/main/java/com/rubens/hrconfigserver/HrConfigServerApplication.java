3b37a86475ec060da447e1356fa41dc68ae21bff
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-03-14 16:04:22 -0300

    Cors Configuration

--- hr-config-server/src/main/java/com/rubens/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/rubens/hrconfigserver/HrConfigServerApplication.java
@@ -10,19 +10,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class HrConfigServerApplication implements CommandLineRunner {
 
-	@Value("${spring.cloud.config.server.git.username}")
-	private String username;
-
-	@Value("${spring.cloud.config.server.git.password}")
-	private String password;
-
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		//System.out.println("USERNAME = " + username);
-		//System.out.println("Pass = " + password);
 	}
 }
